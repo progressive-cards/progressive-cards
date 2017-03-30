d636e2fe055f2133d2607cad44fd4abbc0229212
T12449 <krishnasagar.r@sonata-software.com>
2017-03-30 11:14:29 +0100

    add multiple mind blocking features

--- src/Cards/Notification.js
+++ src/Cards/Notification.js
@@ -1,18 +1,25 @@
 import React from 'react'
+import Ripple from 'react-ink'
 
 export default class Notification extends React.Component {
 
   render(){
     return (<div className="mdl-cell mdl-cell--12-phone mdl-shadow--2dp" draggable="true" >
-             <div className="mdl-card notification-card ">
-              <div className="mdl-card__title">
-                <h2 className="mdl-card__title-text">title Text Goes Here</h2>
-              </div>
-              <div className="mdl-card__supporting-text">
-                This text might describe the photo and provide further information, such as where and
-                when it was taken.
-              </div>
-            </div>
+
+             <div className="mdl-card engagement-card " >
+               <Ripple></Ripple>
+               <div className="mdl-card__title">
+                 <div>
+                   <span className="mdl-typography--subhead" > Notification </span>
+                   <h3>{ this.props.title }</h3>
+                 </div>
+                 <div className="image" ><img src={ this.props.image } alt="London" /></div>
+               </div>
+               <p className="mdl-card__supporting-text" >
+                 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                 Nulla malesuada faucibus mauris, quis finibus metus mollis vitae.
+               </p>
+               </div>
             </div>
             );
   }
